82b044ef551f09882f1a6b54308f5021bf3f741e
lelosio <113931027+lelosio@users.noreply.github.com>
2024-04-21 15:16:43 +0200

    background scroll + player movment

---
+++ Assets/Backgroundscroller.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Backgroundscroller : MonoBehaviour
+{
+   public BoxCollider2D collider;
+   public Rigidbody2D rb;
+   private float width;
+   private float scrollSpeed = -2f; 
+
+    // Update is called once per frame
+    void Start()
+    {
+       collider = GetComponent<BoxCollider2D>();
+       rb = GetComponent<Rigidbody2D>();
+
+       width = collider.size.x;
+       collider.enabled = false;
+
+       rb.velocity = new Vector2(scrollSpeed, 0);
+       ResetObstacle();
+    }
+
+    void update()
+    {
+        if (transform.position.x < -width)
+        {
+            Vector2 resetPosition = new Vector2(width * 2f, 0);
+            transform.position = (Vector2)transform.position + resetPosition;
+            ResetObstacle();
+        }
+    }
+    void ResetObstacle()
+    {
+        transform.GetChild(0).localPosition = new Vector3(0, Random.Range(-3,3), 0 );
+    }
+
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveDirection = Input.GetAxisRaw("Vertical");
+        rb.velocity = new Vector2(0, moveDirection * moveSpeed);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Obstacle")){
+            Destroy(gameObject);
+        }
+    }
+}
